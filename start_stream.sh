de712e100e5f421f657104e26b4e8ce68e259da8
Indexmod <23720911+indexmod@users.noreply.github.com>
2024-11-03 21:01:01 +0300

    обновляем файлы и скрипт

--- start_stream.sh
+++ start_stream.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
+# Задайте переменные для подключения
+ICECAST_URL="icecast://source:ваш_пароль@localhost:8000/stream"
+AUDIO_PATH="assets/audio"
+
 # Бесконечный цикл для автоматического вещания
 while true; do
     # Определяем текущий час (от 0 до 23)
     current_hour=$(date +"%H")
 
     # Формируем имя файла для текущего часа
-    audio_file="assets/audio/hour${current_hour}.mp3"
+    audio_file="${AUDIO_PATH}/hour${current_hour}.mp3"
 
     # Проверка на существование файла перед запуском
     if [[ -f "$audio_file" ]]; then
         echo "Playing $audio_file"
 
         # Запуск ffmpeg с выбранным файлом
-        ffmpeg -re -i "$audio_file" -f mp3 http://localhost:8000/stream
+        ffmpeg -re -i "$audio_file" -f mp3 "$ICECAST_URL" || {
+            echo "Ошибка при воспроизведении $audio_file. Повтор через минуту."
+            sleep 60  # Ждем минуту перед повтором
+            continue
+        }
     else
         echo "File $audio_file does not exist. Skipping."
     fi
